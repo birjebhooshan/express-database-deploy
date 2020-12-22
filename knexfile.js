724e017f2165a7750c48e799930f8ba5c72befa9
Bhooshan <me.45.birjebhooshan@gmail.com>
2020-12-22 20:29:46 +0530

    added production value to knex file

--- knexfile.js
+++ knexfile.js
@@ -8,5 +8,17 @@ module.exports = {
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_USER_PW
     }
+  },
+  production: {
+    client: "postgresql",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tablename: "knex_migrations",
+      directory: "./migrations"
+    }
   }
 };
