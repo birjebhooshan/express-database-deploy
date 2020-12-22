a380e58194c26a908f308dea44f88b6eba9447df
Bhooshan <me.45.birjebhooshan@gmail.com>
2020-12-23 01:53:44 +0530

    add dotenv to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,13 @@
 // Update with your config settings.
-
+require("dotenv").config();
 module.exports = {
   development: {
     client: "postgresql",
     connection: {
-      database: process.env.POSTGRES_DATABASE,
+      host: "localhost",
       user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_USER_PW
+      password: process.env.POSTGRES_USER_PW,
+      database: process.env.POSTGRES_DATABASE
     }
   },
   production: {
